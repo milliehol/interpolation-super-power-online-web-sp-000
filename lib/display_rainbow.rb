31e61cf58c6521c2d1176406d165015027e499c2
Carla Holcomb <carla.programmer@gmail.com>
2019-09-14 19:05:20 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,6 @@
 # Write your #display_rainbow method here
 
 def display_rainbow(colors)
-  puts "Name: #{person[0]}, Age: #{person[1]}, Occupation: #{person[2]}"
+  puts "Name: #{colors[0]}, Age: #{person[1]}, Occupation: #{person[2]}"
   puts "Contact: #{person[3]}"
 end
